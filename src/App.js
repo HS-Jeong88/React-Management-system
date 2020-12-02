15481fbf7b7651e21e05095de890c72ffa82dbe5
정한새 <jhs@jeonghansaeui-MacBookPro.local>
2020-12-02 17:18:26 +0900

    React-Management-System

--- src/App.js
+++ src/App.js
@@ -1,19 +1,81 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
-import React, {Component} from 'react';
-import axios from "axios";
-import { computeHeadingLevel } from '@testing-library/react';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <header className="App-header">
-          <h2>Let's develop management system!</h2>
-        </header>
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.uit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  }, {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '나동빈',
+    'birthday': '960508',
+    'gender': '남자',
+    'job': '프로그래머'
+  }, {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday': '961127',
+    'gender': '남자',
+    'job': '디자이너'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map(c => {
+                return <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job} />
+              })
+            }
+          </TableBody>
+        </Table>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
